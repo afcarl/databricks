b1a9f5a5faf816dc6c44fa3b3d22712131b9d14b
yoav freund <yoav.freund@gmail.com>
2015-09-08 15:25:40 -0700

    A notebook for demonstrating how to process the weather data in DataBricks.

---
+++ notebooks/Users/yfreund@ucsd.edu/Processing Weather.py
@@ -0,0 +1,5 @@
+# Databricks notebook source exported at Tue, 8 Sep 2015 22:25:39 UTC
+# MAGIC %md ### This notebook demonstrates how to process the full NCDC weather dataset inside spark.
+
+# COMMAND ----------
+
