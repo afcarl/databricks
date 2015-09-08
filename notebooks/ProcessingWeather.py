ae8274e4921472d48566adcc7da3bf6b9142e8ff
yoav freund <yfreund@ucsd.edu>
2015-09-08 15:42:59 -0700

    working on notebooks in emacs

--- notebooks/ProcessingWeather.py
+++ notebooks/ProcessingWeather.py
@@ -1,5 +1,19 @@
 # Databricks notebook source exported at Tue, 8 Sep 2015 22:31:43 UTC
+
+# MAGIC %run /Users/yfreund@ucsd.edu/Vault
+
+# COMMAND ----------
+
 # MAGIC %md ### This notebook demonstrates how to process the full NCDC weather dataset inside spark.
 
 # COMMAND ----------
 
+dbutils.fs.unmount("/mnt/%s" % MOUNT_NAME)
+dbutils.fs.mount("s3n://%s:%s@%s" % (ACCESS_KEY, ENCODED_SECRET_KEY, AWS_BUCKET_NAME), "/mnt/%s" % MOUNT_NAME)
+
+# COMMAND ----------
+
+file_list=dbutils.fs.ls('/mnt/NCDC-weather/WeatherUncompressed/')
+[file.path for file in file_list[:3]]
+
+# COMMAND ----------
